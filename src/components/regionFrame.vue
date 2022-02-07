eed48e48148e950dfb154c472d85a59f34711f09
Simon Gaufreteau <simon.gaufreteau@gmail.com>
2022-02-07 16:01:39 +0100

    Adds detailed regions info

--- src/components/regionFrame.vue
+++ src/components/regionFrame.vue
@@ -1,5 +1,19 @@
 <template>
   <div>Current region is : {{ current_region }}</div>
+  <div class="total_conso">
+    Conso Totale (MW) : {{ current_eco2mix_data["Total"] }}
+  </div>
+  <div>
+    <ul>
+      <div class="underlined">Production (MW) :</div>
+      <li>Thermique : {{ current_eco2mix_data["Thermique"] }}</li>
+      <li>Nucléaire : {{ current_eco2mix_data["Nucléaire"] }}</li>
+      <li>Éolien : {{ current_eco2mix_data["Éolien"] }}</li>
+      <li>Solaire : {{ current_eco2mix_data["Solaire"] }}</li>
+      <li>Hydraulique : {{ current_eco2mix_data["Hydraulique"] }}</li>
+      <li>Bioénergies : {{ current_eco2mix_data["Bioénergies"] }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,11 +25,19 @@ export default {
         return this.$store.state.current_region;
       },
     },
+    current_eco2mix_data: {
+      get() {
+        return this.$store.state.eco2mix_data.get(this.current_region);
+      },
+    },
   },
   methods: {
     fetchData() {
       if (this.$store.state.current_region == null) {
         this.$store.dispatch("fetchCurrentRegion");
+      }
+      if (this.$store.state.eco2mix_data == null) {
+        this.$store.dispatch("fetchECO2MIX");
       }
     },
   },
