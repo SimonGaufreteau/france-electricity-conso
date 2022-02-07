e73c78869d159537954bc12457ea8ca011383002
Simon Gaufreteau <simon.gaufreteau@gmail.com>
2022-02-07 15:19:26 +0100

    Adds personalized region frame

---
+++ src/components/regionFrame.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>Current region is : {{ current_region }}</div>
+</template>
+
+<script>
+export default {
+  name: "regionFrame",
+  computed: {
+    current_region: {
+      get() {
+        return this.$store.state.current_region;
+      },
+    },
+  },
+  methods: {
+    fetchData() {
+      if (this.$store.state.current_region == null) {
+        this.$store.dispatch("fetchCurrentRegion");
+      }
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
+
+<style>
+</style>
